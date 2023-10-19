dd6ce7eb426496238ce6000a590e3dc0de99cfb1
Stanislav Gusev <st.al.gusev@gmail.com>
2023-10-19 17:31:23 +0300

    Added registration of customer orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from cart.cart import Cart
+from .models import OrderItem
+from .forms import OrderCreateForm
 
-# Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price = item['price'],
+                                         quantity = item['quantity'])
+            # очистить корзину
+            cart.clear()
+            return render(request,
+                            'orders/order/created.html',
+                            {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request,
+                    'orders/order/create.html',
+                    {'cart': cart, 'form': form})
